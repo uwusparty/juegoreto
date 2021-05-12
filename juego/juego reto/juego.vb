76fbb27d1af95ead447eed2e7a5929a1a6ea9c92
uwusparty <uwusparty@gmail.com>
2021-05-12 13:05:52 +0200

    preguntas API

--- juego/juego reto/juego.vb
+++ juego/juego reto/juego.vb
@@ -1,14 +1,34 @@
-﻿Public Class Juego
+﻿Imports System.Net
+Imports Newtonsoft.Json
+Imports System.Text
+Public Class Juego
     Dim eleccion As Integer
     Dim contador As Integer
     Dim preguntas As Integer
     Dim puntuacion As Integer
     Dim puntuaciones As puntuaciones
+    Dim n As WebClient = New WebClient()
+    Public arrayPreg As List(Of String) = New List(Of String)
+    Dim posicion As Integer = 0
+    Dim guardarPreguntas As List(Of String)
+    Dim random As Integer
 
+    Public Function getPreguntas() As List(Of String)
+        Dim json = n.DownloadString("http://192.168.6.218:8080/trivialmi/questions")
+        Dim read = Linq.JObject.Parse(json)
+        Dim arrayPreguntas As List(Of String) = New List(Of String)
+        For i As Integer = 0 To read.Item("data").Count - 1
+            arrayPreguntas.Add(read.Item("data")(i).ToString)
+        Next
+        Return arrayPreguntas
+    End Function
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles timerImagen.Tick
         contador += 1
         If preguntas < 25 Then
-            If contador = 5 Then
+            If contador = 0 Then
+                random = CInt(Int((guardarPreguntas.Count * Rnd()) + 0))
+                MessageBox.Show(random)
+            ElseIf contador = 5 Then
                 picFoto.Hide()
                 lblPregunta.Show()
                 btnRespuesta1.Show()
@@ -34,6 +54,8 @@
 
     Private Sub juego_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         timerImagen.Start()
+        guardarPreguntas = getPreguntas()
+        Randomize()
     End Sub
 
     Private Sub btnRespuesta1_Click(sender As Object, e As EventArgs) Handles btnRespuesta1.Click
@@ -56,4 +78,5 @@
             contador = 20
         End If
     End Sub
+
 End Class
